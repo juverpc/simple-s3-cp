a9d21401e69e5c3eccfaf1e549e52847ce353815
juverpc <74871399+juverpc@users.noreply.github.com>
2021-09-02 13:38:30 -0500

    Update s3_script.sh

--- s3_script.sh
+++ s3_script.sh
@@ -5,6 +5,6 @@ CSV=${1:-'restaurant.csv'}
 for line in $(cat ${CSV} | grep -v '#')
 do
     folder="$(echo $line | cut -d , -f 1)"    
-    aws s3 cp s3://juver-upload-to-s3/model2/${folder}/ /c/Users/jose.portillo/Downloads/test_s3_script/save_s3_files/${folder} --profile default --recursive 
+    aws s3 cp s3://juver-upload-to-s3/model2/${folder}/ lambdas/${folder}/model/ --profile default --recursive 
     echo ${bucket_path} ${file_location}
 done
