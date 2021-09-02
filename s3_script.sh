c42ac159bb738fd89fe7240be0bf92485e429c27
Jose Portillo Cruz <jose.portillo@globant.com>
2021-09-02 16:38:57 -0500

    updating18

--- s3_script.sh
+++ s3_script.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 CSV=${1:-'restaurant.csv'}
-
+echo "start"
 #Chal
 for line in $(cat ${CSV} | grep -v '#')
 do
     restaurant="$(echo $line | cut -d , -f 1)"    
     aws s3 cp s3://juver-upload-to-s3/model2/${restaurant}/chal/ ./lambdas/${restaurant}-chal/model/ --recursive 
-    
+    echo "first Do"
 done
 
 #Champ
@@ -15,5 +15,7 @@ for line in $(cat ${CSV} | grep -v '#')
 do
     restaurant="$(echo $line | cut -d , -f 1)"    
     aws s3 cp s3://juver-upload-to-s3/model2/${restaurant}/champ/ ./lambdas/${restaurant}-champ/model/ --recursive 
-    
+    echo "Second DO"
 done
+
+echo "finish"
