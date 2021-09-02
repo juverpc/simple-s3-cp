97e40a86d6862810d6d5c0ac8c55561e1327782b
Jose Portillo Cruz <jose.portillo@globant.com>
2021-09-02 14:18:36 -0500

    updating10

--- lambdas/restaurant2/lambda_function.py
+++ lambdas/restaurant2/lambda_function.py
@@ -1,2 +1,2 @@
 def lambda_handler(event, context):
-    print('hello from restaurant2')
+    print('hello from restaurant2!!')
--- s3_script.sh
+++ s3_script.sh
@@ -4,7 +4,7 @@ CSV=${1:-'restaurant.csv'}
 
 for line in $(cat ${CSV} | grep -v '#')
 do
-    folder="$(echo $line | cut -d , -f 1)"    
-    aws s3 cp s3://juver-upload-to-s3/model2/${folder}/ ${CODEBUILD_SRC_DIR}/lambdas/${folder}/model/ --recursive 
+    restaurant="$(echo $line | cut -d , -f 1)"    
+    aws s3 cp s3://juver-upload-to-s3/model2/${restaurant}/ ./lambdas/${restaurant}/model/ --recursive 
     echo ${bucket_path} ${file_location}
 done
