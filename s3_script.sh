ff7750aaceec92beb12d12f273e629739009949e
Jose Portillo Cruz <jose.portillo@globant.com>
2021-09-06 15:29:29 -0500

    updating

--- s3_script.sh
+++ s3_script.sh
@@ -2,18 +2,13 @@
 
 CSV=${1:-'restaurant.csv'}
 
-#Chal
 for restaurant in $(cat ${CSV} | grep -v '#')
 do  
-    aws s3 cp s3://juver-upload-to-s3/model2/${restaurant}/chal/ ./lambdas/${restaurant}-chal --recursive
+    aws s3 cp s3://juver-upload-to-s3/model2/${restaurant}/chal/ ./lambdas/${restaurant}-chal/model/ --recursive
+    aws s3 cp s3://juver-upload-to-s3/model2/${restaurant}/champ/ ./lambdas/${restaurant}-champ/model/ --recursive
     
 done
 
-#Champ
-for restaurant in $(cat ${CSV} | grep -v '#')
-do  
-    aws s3 cp s3://juver-upload-to-s3/model2/${restaurant}/champ/ ./lambdas/${restaurant}-champ/model/ --recursive 
-    
-done
+
 
 
