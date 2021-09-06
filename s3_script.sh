bbfc2bcee10cb6461148542477ef72325786de2a
Jose Portillo Cruz <jose.portillo@globant.com>
2021-09-06 16:48:32 -0500

    update

--- s3_script.sh
+++ s3_script.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
+#Variables
 CSV=${1:-'./environment/dev/restaurant.csv'}
-YML=${2:-'./environment/dev/config.yml'}
 S3_BUCKET=$(grep -A0 'data_bucket:' ./environment/dev/config.yml | tail -n1); S3_BUCKET=${S3_BUCKET//*data_bucket: /}
 MODEL_PATH=$(grep -A0 'model_path:' ./environment/dev/config.yml | tail -n1); MODEL_PATH=${MODEL_PATH//*model_path: /}
-for restaurant in $(cat ${CSV} | grep -v '#')
+#**********************************************#
+
+for RESTAURANT in $(cat ${CSV} | grep -v '#')
 do  
-    aws s3 cp s3://$S3_BUCKET/$MODEL_PATH/${restaurant}/chal/ ./lambdas/${restaurant}-chal/model/ --recursive
-    aws s3 cp s3://$S3_BUCKET/$MODEL_PATH/${restaurant}/champ/ ./lambdas/${restaurant}-champ/model/ --recursive
+    aws s3 cp s3://$S3_BUCKET/$MODEL_PATH/${RESTAURANT}/chal/ ./lambdas/${RESTAURANT}-chal/model/ --recursive
+    aws s3 cp s3://$S3_BUCKET/$MODEL_PATH/${RESTAURANT}/champ/ ./lambdas/${RESTAURANT}-champ/model/ --recursive
     # aws s3 cp s3://juver-upload-to-s3/model/${restaurant}/chal/ ./lambdas/${restaurant}-chal/model/ --recursive
     # aws s3 cp s3://juver-upload-to-s3/model/${restaurant}/champ/ ./lambdas/${restaurant}-champ/model/ --recursive
     
-done < restaurant.csv
+done < ./environment/dev/restaurant.csv
 
 
 
