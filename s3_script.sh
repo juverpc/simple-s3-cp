cb70a5a3d067bda56337095512109116a594d3d3
Jose Portillo Cruz <jose.portillo@globant.com>
2021-09-06 15:59:15 -0500

    update

--- s3_script.sh
+++ s3_script.sh
@@ -7,7 +7,7 @@ do
     aws s3 cp s3://juver-upload-to-s3/model2/${restaurant}/chal/ ./lambdas/${restaurant}-chal/model/ --recursive
     aws s3 cp s3://juver-upload-to-s3/model2/${restaurant}/champ/ ./lambdas/${restaurant}-champ/model/ --recursive
     
-done
+done < restaurant.csv
 
 
 
