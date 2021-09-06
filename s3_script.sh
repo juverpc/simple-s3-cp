d2e1d38bf1c5d26a0d17ad73fb16cec1a52968e7
juverpc <74871399+juverpc@users.noreply.github.com>
2021-09-06 12:28:34 -0500

    Update s3_script.sh

--- s3_script.sh
+++ s3_script.sh
@@ -3,10 +3,9 @@
 CSV=${1:-'restaurant.csv'}
 
 #Chal
-aws s3 ls
 for restaurant in $(cat ${CSV} | grep -v '#')
 do  
-    aws s3 cp s3://juver-upload-to-s3/model2/${restaurant}/chal/ ./ --recursive #./lambdas/${restaurant}-chal --recursive 
+    aws s3 cp s3://juver-upload-to-s3/model2/${restaurant}/chal/ ./lambdas/${restaurant}-chal --recursive 
     
 done
 
