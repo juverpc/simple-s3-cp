b570bb7a3fdd5db3aea5ec8921402ae61b75de81
Jose Portillo Cruz <jose.portillo@globant.com>
2021-09-07 15:33:17 -0500

    update

--- s3_cp.py
+++ s3_cp.py
@@ -9,15 +9,14 @@ def read_yaml_file():
         model_path= (get_value['model_path'])
         locations= (get_value['locations'])
     for restaurant in locations:
+       #CHALLENGER
         os.system("aws s3 cp s3://"+str(data_bucket)+"/"+str(model_path)+"/"+str(restaurant)+"/chal/"+" "+ "./lambdas/"+str(restaurant)+"-chal/model/"+" "+ "--recursive")
         os.system("tar xzvf"+" "+"./lambdas/"+str(restaurant)+"-chal/model/test.tar.gz"+" "+"-C"+" "+"./lambdas/"+str(restaurant)+"-chal/model/")
         os.system("rm -rf" +" "+"./lambdas/"+str(restaurant)+"-chal/model/test.tar.gz")
-       
-        
+       #CHAMPION        
         os.system("aws s3 cp s3://"+str(data_bucket)+"/"+str(model_path)+"/"+str(restaurant)+"/champ/"+" "+ "./lambdas/"+str(restaurant)+"-champ/model/"+" "+ "--recursive")
         os.system("tar xzvf"+" "+"./lambdas/"+str(restaurant)+"-champ/model/test.tar.gz"+" "+"-C"+" "+"./lambdas/"+str(restaurant)+"-champ/model/")
         os.system("rm -rf" +" "+"./lambdas/"+str(restaurant)+"-champ/model/test.tar.gz")
-        
-
+     
 if __name__ == "__main__":
     read_yaml_file()
--- s3_script.sh
+++ s3_script.sh
@@ -3,20 +3,17 @@
 #Variables
 #CSV=${1:-'./environment/dev/restaurant.csv'}
 
-LOCATIONS=$(grep -A0 'locations:' ./environment/dev/config.yml | tail -n2); LOCATIONS=${LOCATIONS//*locations: /}
-echo $LOCATIONS
-# DATA_BUCKET=$(grep -A0 'data_bucket:' ./environment/dev/config.yml | tail -n1); DATA_BUCKET=${DATA_BUCKET//*data_bucket: /}
-# MODEL_PATH=$(grep -A0 'model_path:' ./environment/dev/config.yml | tail -n1); MODEL_PATH=${MODEL_PATH//*model_path: /}
 
-# for RESTAURANT in $(cat ${CSV} | grep -v '#')
-# do  
+DATA_BUCKET=$(grep -A0 'data_bucket:' ./environment/dev/config.yml | tail -n1); DATA_BUCKET=${DATA_BUCKET//*data_bucket: /}
+MODEL_PATH=$(grep -A0 'model_path:' ./environment/dev/config.yml | tail -n1); MODEL_PATH=${MODEL_PATH//*model_path: /}
 
-#     aws s3 cp s3://$DATA_BUCKET/$MODEL_PATH/${RESTAURANT}/chal/ ./lambdas/${RESTAURANT}-chal/model/ --recursive
-#     descom
-#     rm
-#     aws s3 cp s3://$DATA_BUCKET/$MODEL_PATH/${RESTAURANT}/champ/ ./lambdas/${RESTAURANT}-champ/model/ --recursive
+for RESTAURANT in $(cat ${CSV} | grep -v '#')
+do  
+
+    aws s3 cp s3://$DATA_BUCKET/$MODEL_PATH/${RESTAURANT}/chal/ ./lambdas/${RESTAURANT}-chal/model/ --recursive
+    aws s3 cp s3://$DATA_BUCKET/$MODEL_PATH/${RESTAURANT}/champ/ ./lambdas/${RESTAURANT}-champ/model/ --recursive
         
-# done < ./environment/dev/restaurant.csv
+done < ./environment/dev/restaurant.csv
 
 
 
