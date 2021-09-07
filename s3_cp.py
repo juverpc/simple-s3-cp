ca207bf15bfeee32f6d7665410fc7aecac2614df
Jose Portillo Cruz <jose.portillo@globant.com>
2021-09-07 14:24:09 -0500

    update

--- s3_cp.py
+++ s3_cp.py
@@ -10,21 +10,14 @@ def read_yaml_file():
         locations= (get_value['locations'])
     for restaurant in locations:
         os.system("aws s3 cp s3://"+str(data_bucket)+"/"+str(model_path)+"/"+str(restaurant)+"/chal/"+" "+ "./lambdas/"+str(restaurant)+"-chal/model/"+" "+ "--recursive")
-        os.system("cd"+" "+"./lambdas/"+str(restaurant)+"-chal/model/")
-        os.system("tar xzvf test.tar.gz")
-        os.system("rm -rf test.tar.gz ")
-        os.system("ls")
-        os.system("cd ..")
-        os.system("cd ..")
-        os.system("cd ..")
+        os.system("tar xzvf"+" "+"./lambdas/"+str(restaurant)+"-chal/model/test.tar.gz"+" "+"-C"+" "+"./lambdas/"+str(restaurant)+"-chal/model/")
+        os.system("rm -rf" +" "+"./lambdas/"+str(restaurant)+"-chal/model/test.tar.gz")
+       
         
         os.system("aws s3 cp s3://"+str(data_bucket)+"/"+str(model_path)+"/"+str(restaurant)+"/champ/"+" "+ "./lambdas/"+str(restaurant)+"-champ/model/"+" "+ "--recursive")
-        os.system("cd"+" "+ "./lambdas/"+str(restaurant)+"-champ/model/")
-        os.system("tar xzvf test.tar.gz")
-        os.system("rm -rf test.tar.gz ")
-        os.system("cd ..")
-        os.system("cd ..")
-        os.system("cd ..")
+        os.system("tar xzvf"+" "+"./lambdas/"+str(restaurant)+"-champ/model/test.tar.gz"+" "+"-C"+" "+"./lambdas/"+str(restaurant)+"-champ/model/")
+        os.system("rm -rf" +" "+"./lambdas/"+str(restaurant)+"-champ/model/test.tar.gz")
+        
 
 if __name__ == "__main__":
     read_yaml_file()
