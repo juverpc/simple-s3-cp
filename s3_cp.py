19906be4e4fc3aa670a296f17382b82ef022fae5
Jose Portillo Cruz <jose.portillo@globant.com>
2021-09-23 10:33:14 -0500

    test2

--- s3_cp.py
+++ s3_cp.py
@@ -1,24 +1,20 @@
 import boto3
 import os
 import tempfile
+import yaml
+from yaml import Loader
 
-S3_BUCKET = "juver-upload-to-s3"
-S3_PREFIX = "auto_training_pipeline/sm_pipelines/deployed_models/restaurant1/challenger_model/stand_menu_2ac31518-47bb-4951-ba8c-c3f058193535/"
-
+#WORK
 def download_object():
-    #with tempfile.TemporaryDirectory() as tempdir:
-    local_path= './lambdas/restaurant1-chal/model/'
-    s3 = boto3.client('s3')
-    response = s3.list_objects_v2(Bucket=S3_BUCKET, Prefix=S3_PREFIX)
+    with open("./environment/dev/config.yml", 'r') as stream:
+        get_config = yaml.load(stream,Loader=Loader)
+        data_bucket= (get_config['data_bucket'])
+        model_path= (get_config['model_path'])
+    s3=boto3.client('s3')
+    response = s3.list_objects_v2(Bucket=data_bucket, Prefix=f'{model_path}/restaurant1/challenger_model/stand_menu_2ac31518-47bb-4951-ba8c-c3f058193535/')
     s3_objects = [obj['Key'] for obj in response['Contents']]
     for s3_file in s3_objects:
-        local_file_path = os.path.join(local_path, s3_file.replace(S3_PREFIX, ""))
-        s3.download_file(S3_BUCKET, s3_file, local_file_path)
-    print(f"Downloaded files: {', '.join(os.listdir(local_path))}")
-#WORK
-# def download_object():
-#     s3=boto3.client('s3')
-#     s3.download_file('juver-upload-to-s3', 'auto_training_pipeline/sm_pipelines/deployed_models/restaurant1/challenger_model/stand_menu_2ac31518-47bb-4951-ba8c-c3f058193535/model.tar.gz', './lambdas/restaurant1-chal/model/model.tar.gz')
+        s3.download_file(data_bucket,s3_file,'./lambdas/restaurant1-chal/model/')
 
 if __name__ == "__main__":
     download_object()
